6699bbbc10165168166811de9621f83f4c2ac689
P360 <hacera11@yahoo.com>
2019-04-15 20:53:01 +0300

    Started adding slider home

--- ListLess/src/app/home/home.page.ts
+++ ListLess/src/app/home/home.page.ts
@@ -1,8 +1,45 @@
 import { Component } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss']
 })
-export class HomePage {}
+export class HomePage {
+  @ViewChild('todo_list') todo_slides: IonSlides;
+
+  todo_slide_options = {
+    effect: 'flip'
+  };
+  
+ todo_list = [
+    {
+      title: 'List1'
+    },
+    {
+      title: 'List2'
+    },
+    {
+      title: 'List3'
+    },
+  ];
+
+  nextSlide() {
+    this.todo_slides.slideNext();
+  }
+
+  prevSlide() {
+    this.todo_slides.slidePrev();
+  }
+
+  getToDoArray(){
+    return this.todo_list;
+  }
+
+  constructor() {
+    //this.todo_list = this.todo_list;
+  }
+
+}
