770e1f45647594e8b5da0f9b6db4a625b0140f82
P360 <hacera11@yahoo.com>
2019-04-16 12:27:19 +0300

    Minor commit

--- ListLess/src/app/home/home.page.ts
+++ ListLess/src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ViewChild } from '@angular/core';
 import { IonSlides } from '@ionic/angular';
+import { Transaction } from '../services/balance.service';
 
 @Component({
   selector: 'app-home',
@@ -9,10 +10,6 @@ import { IonSlides } from '@ionic/angular';
 })
 export class HomePage implements OnInit{
 
-
-  ngOnInit(): void {
-    
-  }
   @ViewChild('todo_list') todo_slides: IonSlides;
   @ViewChild('shopping_list') shopping_slides: IonSlides;
   @ViewChild('balance_list') balance_slides: IonSlides;
@@ -20,7 +17,7 @@ export class HomePage implements OnInit{
   slide_options = {
     effect: 'flip'
   };
-  
+  slide_obj: any;
  todo_list = [
     {
       title: 'List1'
@@ -33,13 +30,60 @@ export class HomePage implements OnInit{
     },
   ];
  
-  nextSlideToDo() {
-    this.todo_slides.slideNext();
+  slideSection(who:number,left:boolean) {
+    switch(who){
+      case 1:{
+        this.slide_obj = this.todo_slides;
+        break;
+      }
+      case 2: {
+        this.slide_obj = this.shopping_slides;
+        break;
+      }
+      case 3: {
+        this.slide_obj = this.balance_slides;
+        break;
+      }
+      default:{
+        this.slide_obj = this.todo_slides;
+      }
+    }
+    
+    if(!left){
+      this.slide_obj.slideNext();
+    }
+    else{
+      this.slide_obj.slidePrev();
+    }
   }
 
-  prevSlideToDo() {
-    this.todo_slides.slidePrev();
-  }
+  balance: any;
+  todo: any;
+  transactions: any;
+
+  ngOnInit(): void { 
+    this.transactions = new Array();
+    this.todo = new Array();
+    this.balance = new Array();
+    let todo_list = [
+      {
+        title: 'List1'
+      },
+      {
+        title: 'List2'
+      },
+      {
+        title: 'List3'
+      },
+    ];
+    this.todo.push(todo_list);
+    this.balance.push(todo_list);
+    this.transactions.push(todo_list);
+
+    console.log(this.todo);
+    console.log(this.balance);
+    console.log(this.transactions);
+ }
 
   constructor() {
     
