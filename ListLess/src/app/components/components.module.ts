71b21a2aac41cf851c86c0dfedb43329b6df7fce
Borislav Nikolov <borislav.nikolov@petroffsoft.bg>
2019-04-16 13:40:52 +0300

    Build fix again

--- ListLess/src/app/components/components.module.ts
+++ ListLess/src/app/components/components.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { TransactionItemComponent } from './balance/transaction-item/transaction-item.component';
-import { TransactionItemsComponent } from './transaction-items/transaction-items.component';
+import { TransactionItemsComponent } from './balance/transaction-items/transaction-items.component';
 
 @NgModule({
   declarations: [TransactionItemComponent, TransactionItemsComponent],
