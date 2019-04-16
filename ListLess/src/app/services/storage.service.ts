93be4399d31dd8d0060dedeba6a0dbf8dbf5bf6b
Nikolay <nikkolay.staykov98@abv.bg>
2019-04-16 13:22:18 +0300

    Services for todo items and transactionitems

--- ListLess/src/app/services/storage.service.ts
+++ ListLess/src/app/services/storage.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
 import { ShoppingItemModel } from './models/shopping-item-model';
+import { TodoItemModel } from './models/todo-item-model';
+import { TransactionItemModel} from './models/transaction-item-model';
+import { promise } from 'protractor';
 
 const ShoppingItemsKey = '1';
 const ToDoListItemsKey = '2';
-const TransactinItemsKey = '3';
+const TransactionItemsKey = '3';
 
 @Injectable({
 	providedIn: 'root'
@@ -18,7 +21,7 @@ export class StorageService {
 		return this.storage.get(ShoppingItemsKey).then((items: ShoppingItemModel[]) => {
 			if (items) {
 				items.push(item);
-				return this.storage.set(ShoppingItemsKey, [ item ]);
+				return this.storage.set(ShoppingItemsKey, items);
 			} else {
 				return this.storage.set(ShoppingItemsKey, [ item ]);
 			}
@@ -28,7 +31,7 @@ export class StorageService {
 	getShoppingItems(): Promise<ShoppingItemModel[]> {
 		return this.storage.get(ShoppingItemsKey);
   }
-  
+
 	// get shopping items for a specific shopping list
 	getShoppinglist(ShoppingListId: number): Promise<ShoppingItemModel[]> {
 		return this.storage.get(ShoppingItemsKey).then((items: ShoppingItemModel[]) => {
@@ -76,5 +79,148 @@ export class StorageService {
 				return this.storage.set(ShoppingItemsKey, newItems);
 			}
 		});
-	}
+  }
+  // add todo item
+  addToDoItem(item: TodoItemModel) : Promise<TodoItemModel>
+  {
+    return this.storage.get(ToDoListItemsKey).then((items: TodoItemModel[]) => {
+			if (items) {
+				items.push(item);
+				return this.storage.set(ToDoListItemsKey, items);
+			} else {
+				return this.storage.set(ToDoListItemsKey, [ item ]);
+			}
+		});
+  }
+  // get todo items
+  getToDoItems() : Promise<TodoItemModel[]> 
+  {
+    return this.storage.get(ToDoListItemsKey);
+  }
+  // get todo items for specific list
+  getToDoItemsForList(ToDoListId: number) : Promise<TodoItemModel[]> 
+  {
+    return this.storage.get(ToDoListItemsKey).then((items: TodoItemModel[]) => {
+			let newItems: TodoItemModel[];
+			for (let i of items) {
+				if (i.parentid === ToDoListId) {
+					newItems.push(i);
+				}
+			}
+			return newItems;
+		});
+  }
+  // update todo item
+  upddateToDoItem(item: TodoItemModel) : Promise<any>
+  {
+    return this.storage.get(ToDoListItemsKey).then((items: TodoItemModel[]) => {
+			if (!items || items.length === 0) {
+				return null;
+			} else {
+				let newItems: TodoItemModel[];
+				for (let i of items) {
+					if (i.id === item.id) {
+						newItems.push(item);
+					} else {
+						newItems.push(i);
+					}
+				}
+				return this.storage.set(ToDoListItemsKey, newItems);
+			}
+		});
+  }
+  // delete a todo item
+  deleteToDoItem(id: number) : Promise<any>
+  {
+    return this.storage.get(ToDoListItemsKey).then((items: TodoItemModel[]) => {
+			if (!items || items.length === 0) {
+				return null;
+			} else {
+				let newItems: TodoItemModel[];
+				for (let i of items) {
+					if (i.id !== id) {
+						newItems.push(i);
+					}
+				}
+				return this.storage.set(ToDoListItemsKey, newItems);
+			}
+		});
+  }
+  //create a transaction item
+  addTransactionItem(item: TransactionItemModel) : Promise<TransactionItemModel>
+  {
+    return this.storage.get(TransactionItemsKey).then((items: TransactionItemModel[]) => {
+			if (items) {
+				items.push(item);
+				return this.storage.set(TransactionItemsKey, items);
+			} else {
+				return this.storage.set(TransactionItemsKey, [ item ]);
+			}
+		});
+  }
+  // get transaction items
+  getTransactionitems() : Promise<TransactionItemModel[]>
+  {
+    return this.storage.get(TransactionItemsKey);
+  }
+  // get transaction items for specific list
+  getTransactionItemsForList(AccountId: number) : Promise<TransactionItemModel[]>
+  {
+    return this.storage.get(TransactionItemsKey).then((items: TransactionItemModel[]) => {
+      let newItems: TransactionItemModel[];
+      for(let i of items)
+      {
+        if(i.parentid === AccountId)
+        {
+          newItems.push(i);
+        }
+      }
+      return newItems;
+		});
+  }
+  //Update Transaction item
+  updateTransactionItem(item: TransactionItemModel) : Promise<any>
+  {
+    return this.storage.get(TransactionItemsKey).then((items: TransactionItemModel[]) => {
+      if(!items || items.length === 0)
+      {
+        return null;
+      }
+      else{
+      let newItems: TransactionItemModel[];
+      for(let i of items)
+      {
+        if(i.id === item.id)
+        {
+          newItems.push(item);
+        }
+        else{
+          newItems.push(i);
+        }
+      }
+      return this.storage.set(TransactionItemsKey,newItems);
+      }
+		});
+  }
+  //Delete TransactionItem
+  deleteTransactionItem(id: number) : Promise<any>
+  {
+    return this.storage.get(TransactionItemsKey).then((items: TransactionItemModel[]) => {
+      if(!items || items.length === 0)
+      {
+        return null;
+      }
+      else{
+      let newItems: TransactionItemModel[];
+      for(let i of items)
+      {
+        if(i.id !== id)
+        {
+          newItems.push(i);
+        }
+      }
+      return this.storage.set(TransactionItemsKey,newItems);
+      }
+		});
+  }
 }
