8d5a1f631e310f7e68cd507d72ca10c31efa3e64
Borislav Nikolov <borislav.nikolov@petroffsoft.bg>
2019-04-16 15:45:56 +0300

    Changed colorValue to an enum and added if in the component

--- ListLess/src/app/balance/balance.page.ts
+++ ListLess/src/app/balance/balance.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { TransactionItemModel } from '../services/models/transaction-item-model';
+import { TransactionItemModel, TRANSACTION_TYPE } from '../services/models/transaction-item-model';
 
 @Component({
 	selector: 'app-balance',
@@ -7,22 +7,36 @@ import { TransactionItemModel } from '../services/models/transaction-item-model'
 	styleUrls: [ 'balance.page.scss' ]
 })
 export class BalancePage implements OnInit {
+	transactions: TransactionItemModel[];
+	title: string;
 
-  transactions: TransactionItemModel[];
-  title: string;
-  
-  ngOnInit(): void {
+	ngOnInit(): void {
 		this.title = 'Cash';
 		this.transactions = new Array();
 		let newtrans = new TransactionItemModel();
 		newtrans.icon = 'card';
 		newtrans.title = 'Item1';
 		newtrans.value = 400;
-		newtrans.valueColor = 'primary';
+		newtrans.transactionType = TRANSACTION_TYPE.TRANSFER;
 		newtrans.id = 0;
 		newtrans.parentid = 0;
 		this.transactions.push(newtrans);
-    this.transactions.push({ id:1, parentid:1, title: 'Item2', icon: 'trash', value: 300, valueColor: 'danger' });
+		this.transactions.push({
+			id: 1,
+			parentid: 1,
+			title: 'Item2',
+			icon: 'appstore',
+			value: 300,
+			transactionType: TRANSACTION_TYPE.EXPENSE
+		});
+		this.transactions.push({
+			id: 1,
+			parentid: 1,
+			title: 'Item3',
+			icon: 'cash',
+			value: 10,
+			transactionType: TRANSACTION_TYPE.INCOME
+		});
 	}
 
 	constructor() {}
--- ListLess/src/app/services/models/transaction-item-model.ts
+++ ListLess/src/app/services/models/transaction-item-model.ts
@@ -1,10 +1,16 @@
 import { BaseItemModel } from './base/base-item-model';
 
+export enum TRANSACTION_TYPE {
+  EXPENSE,
+  INCOME,
+  TRANSFER
+}
+
 export class TransactionItemModel implements BaseItemModel {
     id: number;
     parentid: number;
     title: string;
     icon: string;
     value: number;
-    valueColor: string;
+    transactionType: TRANSACTION_TYPE;
   }
